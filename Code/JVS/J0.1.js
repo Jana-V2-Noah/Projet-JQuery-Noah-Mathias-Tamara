30ff17738bd6f20b0a3e0fb2fa939aab8b3b82b7
Tamaradama <148735244+Tamaradama@users.noreply.github.com>
2024-03-09 11:20:51 -0500

    Update J0.1.js

--- Code/JVS/J0.1.js
+++ Code/JVS/J0.1.js
@@ -1,6 +1,10 @@
  $(document).ready(function () {
     const questions = [
-
+        { question1: "Quelle est la definition de la grandeur physique ?", options: ["Caracteristique defini par une norme et unite", "Caracteristique d'un objet ou phenomene pouvant etre mesure", "Caracteristique defini par une norme et direction (vecteur)", "oui"], answer: "Caracteristique d'un objet ou phenomene pouvant etre mesure" },
+        { question2: "Qu'est ce qu'une force pressante?", options: ["une force modeliser une action mecanique de contacte exerce par un solide ", "Il s’agit d’une force de poussée exercée lors du contact entre un fluide (un gaz ou un liquide) et un autre corps", "Dosage en volume des solutions", "l'interaction physique responsable de l'attraction des corps massifs"], answer: "Il s’agit d’une force de poussée exercée lors du contact entre un fluide (un gaz ou un liquide) et un autre corps" },
+        { question3: "les lignes vectorielles sont", options: ["les courbes obtenues en reliant les points où la grandeur physique étudiée à la même valeur. ", "le vecteur qui le caractérise est le même en tout point de région de l’espace étudiée : même direction, même sens et même valeur", "les courbes tangentes au vecteur champ en chacun de leurs points"], answer: "les courbes tangentes au vecteur champ en chacun de leurs points" },
+        { question4: "Quel objet est tombé sur la tete de Newton", options: ["Une pomme ", "une orange", "une brique"], answer: "une pomme " },
+        { question5: "Que donne le melange d'une lumiere rouge avec une lumiere verte", options: ["mangenta", "cyan", "jaune"], answer: "jaune" },
 
     ];
 
