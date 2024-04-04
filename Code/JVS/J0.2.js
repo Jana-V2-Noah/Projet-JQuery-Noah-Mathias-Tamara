9f5ec785d49735755f4dab366933fb6cd26e6e39
Jana-V2-Noah <156347604+Jana-V2-Noah@users.noreply.github.com>
2024-04-03 20:26:50 -0400

    Add files via upload

--- Code/JVS/J0.2.js
+++ Code/JVS/J0.2.js
@@ -13,10 +13,10 @@ $(document).ready(function () {
     // Define quiz questions
     const questions = [
         { question: "Quelle est la definition de la grandeur physique ?", options: ["Caracteristique defini par une norme et unite", "Caracteristique d'un objet ou phenomene pouvant etre mesure", "Caracteristique defini par une norme et direction (vecteur)", "oui"], answer: "Caracteristique d'un objet ou phenomene pouvant etre mesure" },
-        { question: "Qu'est ce qu'une force pressante?", options: ["une force modeliser une action mecanique de contacte exerce par un solide ", "Il s’agit d’une force de poussée exercée lors du contact entre un fluide (un gaz ou un liquide) et un autre corps", "Dosage en volume des solutions", "l'interaction physique responsable de l'attraction des corps massifs"], answer: "Il s’agit d’une force de poussée exercée lors du contact entre un fluide (un gaz ou un liquide) et un autre corps" },
-        { question: "les lignes vectorielles sont", options: ["les courbes obtenues en reliant les points où la grandeur physique étudiée à la même valeur. ", "le vecteur qui le caractérise est le même en tout point de région de l’espace étudiée : même direction, même sens et même valeur", "les courbes tangentes au vecteur champ en chacun de leurs points"], answer: "les courbes tangentes au vecteur champ en chacun de leurs points" },
-        { question: "Quel objet est tombé sur la tete de Newton", options: ["Une pomme ", "une orange", "une brique"], answer: "Une pomme " },
-        { question: "Que donne le melange d'une lumiere rouge avec une lumiere verte", options: ["mangenta", "cyan", "jaune"], answer: "jaune" },
+        { question: "Qu'est ce qu'une force pressante?", options: ["Une force modeliser une action mecanique de contacte exerce par un solide ", "Il s’agit d’une force de poussée exercée lors du contact entre un fluide (un gaz ou un liquide) et un autre corps", "Dosage en volume des solutions", "L'interaction physique responsable de l'attraction des corps massifs"], answer: "Il s’agit d’une force de poussée exercée lors du contact entre un fluide (un gaz ou un liquide) et un autre corps" },
+        { question: "Les lignes vectorielles sont", options: ["Les courbes obtenues en reliant les points où la grandeur physique étudiée à la même valeur. ", "Le vecteur qui le caractérise est le même en tout point de région de l’espace étudiée : même direction, même sens et même valeur", "Les courbes tangentes au vecteur champ en chacun de leurs points"], answer: "Les courbes tangentes au vecteur champ en chacun de leurs points" },
+        { question: "Quel objet est tombé sur la tete de Newton", options: ["Une pomme ", "Une orange", "Une brique"], answer: "Une pomme " },
+        { question: "Que donne le melange d'une lumiere rouge avec une lumiere verte", options: ["Mangenta", "Cyan", "Jaune"], answer: "Jaune" },
     ];
 
     const quizContainer = $("#question-container");
